b901ac75239d2b30c1ec869b40c38b7db7f7e903
Yuri <yur0n@yahoo.com>
2021-07-18 14:43:34 +0300

    server added

--- bot.js
+++ bot.js
@@ -2,47 +2,63 @@ const botgram = require("botgram")
 const request = require('request')
 const bot = botgram("1883716013:AAF6aaTD5CntA2t7IeL3VGfdBmpqQuSCYV8")
 
-bot.command("start", (msg, reply, next) => {
-    console.log(msg.from.username || msg.from.name, msg.from.id)
-    reply.text('LOADING...')
-    msg.url = 'https://api.opendota.com/api/players/120202499/recentMatches'
-    request({ url: msg.url, json: true }, (error, response) => {
-        if (error) {
-            reply.text('Случилось дерьмо')
-        }
-        msg.matchId = response.body[0].match_id
-        reply.editText(msg.id + 1, 'В последней игре у тебя ' + response.body[0].kills + ' убийства, ' + 
-                       response.body[0].deaths + ' смертей, ' + response.body[0].assists + ' ассистов.')
-        stats(reply, msg, response)
-    })
-    next()
-})
+var key = [
+    [ { text: "Рудя"}, {text: 'Грызля'} ],
+    [ { text: "Антон"}, {text: 'Юрон'} ]
+]
 
-bot.command("start", (msg, reply, next) => {
-    const interval = setInterval(function() {
-        request({ url: msg.url, json: true }, (error, response) => {
-            if (error) {
-                reply.text('Случилось дерьмо')
-            }
-            if (msg.matchId !== response.body[0].match_id) {
-                msg.matchId = response.body[0].match_id
-                reply.text('В последней игре у тебя ' + response.body[0].kills + ' убийства, ' + 
-                           response.body[0].deaths + ' смертей, ' + response.body[0].assists + ' ассистов.')
-                stats(reply, msg, response)
-            }
-        })
-    }, 900000);  
-})
+const loading = ['loading..', 'loading...', 'loading....', 'loading.....', 'loading......']
 
-const stats = (reply, response) => {
+const stats = (reply, msg, response) => {
     reply.text('Так же ты нанёс ' + response.body[0].hero_damage + ' урона по героям, ' + response.body[0].tower_damage + 
                ' урона по строениям, похилил на ' + response.body[0].hero_healing + ' хп и получал ' + 
-               response.body[0].gold_per_min + ' золота/мин с ' + response.body[0].last_hits + ' ластхитами')
-               if (response.body[0].kills <= response.body[0].deaths) reply.text('Слабак!')
-               if (response.body[0].kills > response.body[0].deaths) reply.text('А ты хорош, продолжай!')
-               if ((response.body[0].player_slot < 4 && response.body[0].radiant_win == 1) || 
-               (response.body[0].player_slot > 4 && response.body[0].radiant_win == 0)) reply.text("Ты победил! \u{1F638}")
-               if ((response.body[0].player_slot > 4 && response.body[0].radiant_win == 1) || 
-               (response.body[0].player_slot < 4 && response.body[0].radiant_win == 0)) reply.text("Ты проиграл! \u{1F63F}")
-               if (response.body[0].leaver_status == 1) reply.text('Хренов ливер!')
+               response.body[0].gold_per_min + ' золота/мин с ' + response.body[0].last_hits + ' ластхитами.')
+            if (response.body[0].kills <= response.body[0].deaths) reply.text('Слабак!')
+            if (response.body[0].kills > response.body[0].deaths) reply.text('А ты хорош, продолжай!')
+            if ((response.body[0].player_slot <= 4 && response.body[0].radiant_win == true) || 
+                  (response.body[0].player_slot > 4 && response.body[0].radiant_win == false)) reply.text("Ты победил! \u{1F638}")
+            if ((response.body[0].player_slot > 4 && response.body[0].radiant_win == true) || 
+                  (response.body[0].player_slot <= 4 && response.body[0].radiant_win == false)) reply.text("Ты проиграл! \u{1F63F}")
+            if (response.body[0].leaver_status == 1) reply.text('Хренов ливер!')
 }
+
+bot.command("start", (msg, reply, next) => {
+    console.log(msg.from.username, msg.from.name, msg.from.id)
+    reply.keyboard(key, true, true).text("Ты кто? Введи свой Steam32 ID, если тебя нет в списке.")
+    reply.text('Я буду присылать статистику твоей каждой сыгранной игры.')
+})
+
+bot.text((msg, reply, next) => {
+    var player = msg.text
+    if (msg.text == "Рудя") player = '120202499'; if (msg.text == "Антон") player = '97938416'
+    if (msg.text == "Грызля") player = '115455869'; if (msg.text == "Юрон") player = '93442227'
+    reply.text('loading.')
+    loading.forEach(load => {
+        reply.editText(msg.id + 1, load)
+    })
+    url = 'https://api.opendota.com/api/players/' + player + '/recentMatches'
+    request({ url: url, json: true }, async (error, response) => {
+        try {
+        matchId = response.body[0].match_id
+        await reply.editText(msg.id + 1, 'В последней игре у тебя ' + response.body[0].kills + ' убийств, ' + 
+                       response.body[0].deaths + ' смертей, ' + response.body[0].assists + ' ассистов.')
+        stats(reply, msg, response)
+        } catch (e) {
+            reply.editText(msg.id + 1, 'Неверный ID или настройки приватности')
+        }
+    })
+    setInterval(() => {
+        request({ url: url, json: true }, (error, response) => {
+            try {
+                if (matchId !== response.body[0].match_id) {
+                    matchId = response.body[0].match_id
+                    reply.text('В последней игре у тебя ' + response.body[0].kills + ' убийства, ' + 
+                            response.body[0].deaths + ' смертей, ' + response.body[0].assists + ' ассистов.')
+                    stats(reply, msg, response)
+                }
+            } catch (e) {
+                reply.text('Случилось дерьмо')
+            }    
+        })
+    }, 900000); 
+})
